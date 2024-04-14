4da143915ded4c8f8f54fbaef44f8af18be95fba
smath <smathewslynch@gmail.com>
2024-04-14 15:55:25 +0100

    Made Getters/Setters, equals and hashcode methods and toString method

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.util.Objects;
 
 public class Task {
     private String owner;
@@ -12,5 +13,54 @@ public class Task {
         this.owner = owner;
         this.description = description;
         this.deadline = deadline;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public void setOwner(String owner) {
+        this.owner = owner;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDate getDeadline() {
+        return deadline;
+    }
+
+    public void setDeadline(LocalDate deadline) {
+        if (deadline == null || deadline.compareTo(LocalDate.now()) <= 0) {
+            throw new IllegalArgumentException("Deadline is invalid");
+        }
+        this.deadline = deadline;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Task task = (Task) o;
+        return Objects.equals(owner, task.owner) && Objects.equals(description, task.description) && Objects.equals(deadline, task.deadline);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(owner, description, deadline);
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "owner='" + owner + '\'' +
+                ", description='" + description + '\'' +
+                ", deadline=" + deadline +
+                '}';
     }
 }

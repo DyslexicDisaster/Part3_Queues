a25a33e1676764f7cc10786a074a74d34b559d3d
smath <smathewslynch@gmail.com>
2024-04-14 15:52:05 +0100

    Created Owner,Description,Deadline variables Created the argument constructor

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class Task {
+import java.time.LocalDate;
+
+public class Task {
+    private String owner;
+    private String description;
+    private LocalDate deadline;
+
+    public Task(String owner, String description, LocalDate deadline) {
+        if (deadline.compareTo(LocalDate.now()) <= 0) {
+            throw new IllegalArgumentException("Deadline must be in the future");
+        }
+        this.owner = owner;
+        this.description = description;
+        this.deadline = deadline;
+    }
 }

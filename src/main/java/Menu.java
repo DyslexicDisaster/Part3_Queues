865633fa42673f8c69f4fea77ed0438da84400f9
smath <smathewslynch@gmail.com>
2024-04-16 12:25:44 +0100

    Created menu variable and showMenu method

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -11,5 +11,22 @@ public class Menu {
         System.out.println("Please enter how big you want to make your queue");
         int maxSize = sc.nextInt();
         BoundedPriorityQueueSet taskQueue = new BoundedPriorityQueueSet(maxSize);
+
+        System.out.println("Task manager successfully created");
+
+        String[] menu = {
+                "1. Add a new Task",
+                "2. View the next Task",
+                "3. Mark the next Task as done",
+                "4. View the number of Tasks remaining.",
+                "5. View the space remaining in the queue",
+                "6. Exit"
+        };
+    }
+    public static void showMenu(String[] options, String title) {
+        System.out.println(title);
+        for (int i = 0; i < options.length; i++) {
+            System.out.println(options[i]);
+        }
     }
 }

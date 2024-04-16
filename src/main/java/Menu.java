70ddf7551381567611dc4af47c4d4cfe32af8bbb
smath <smathewslynch@gmail.com>
2024-04-16 12:17:03 +0100

    Created the starting variables Scanner,Random,isRisky.
    Made user enter maxSzie of queue and create said queue

--- src/main/java/Menu.java
+++ src/main/java/Menu.java
@@ -1,5 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Menu {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Random rd = new Random();
+        //Used for methods later
+        boolean isRisky = rd.nextBoolean();
 
+        System.out.println("Please enter how big you want to make your queue");
+        int maxSize = sc.nextInt();
+        BoundedPriorityQueueSet taskQueue = new BoundedPriorityQueueSet(maxSize);
     }
 }

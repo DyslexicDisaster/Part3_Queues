e4e938439e4bf62bd88a5ed0ab7d5944e43e0a7b
MrMalikO <muhammadmalikomer@gmail.com>
2024-04-17 17:21:40 +0100

    Added Javadocs to BoundedPriorityQueueSet methods

--- src/main/java/BoundedPriorityQueueSet.java
+++ src/main/java/BoundedPriorityQueueSet.java
@@ -7,6 +7,7 @@ public class BoundedPriorityQueueSet {
     private final int MAX_SIZE;
 
     public BoundedPriorityQueueSet(int maxSize) {
+        //preventing maxsize form being less than or equal to zero
         if(maxSize <= 0){
             throw new IllegalArgumentException("The max size must be a positive number");
         }
@@ -19,22 +20,45 @@ public class BoundedPriorityQueueSet {
         queue = new LinkedList<Task>();
     }
 
+    /**
+     * checks number of elements in the queue
+     * @return number of elements
+     */
     public int size(){
         return queue.size();
     }
 
+    /**
+     * Returns true or false depending on if queue is empty
+     * @return true if empty or false if not empty
+     */
     public boolean isEmpty(){
         return queue.isEmpty();
     }
 
+    /**
+     * check to see if element being entered is already in queue
+     * @param task Task element to be checked for in the queue
+     * @return true if task already in queue or false otherwise.
+     */
     public boolean isDuplicate(Task task){
         return queue.contains(task);
     }
 
+    /**
+     * Check to see if queue is full
+     * @return true if queue is full or false if not full
+     */
     public boolean isFull(){
         return queue.size()==MAX_SIZE;
     }
 
+    /**
+     * gives position at which task entered should be put into queue
+     * @param task Task to be entered into queue
+     * @return int which represents where in queue task should be inserted
+     * @throws DuplicateElementException If task already in queue
+     */
     private int calcPosition(Task task) throws DuplicateElementException {
         if (isFull()){
             return -1;
@@ -51,12 +75,17 @@ public class BoundedPriorityQueueSet {
             if (task.compareTo(queue.get(i)) < 0){
                 return i;
             } /*else if (task.compareTo(queue.get(i)) > 0) {
-                return i+1;
+                return i+1;             //useless code so was removed
             }*/
         }
         return queue.size();
     }
 
+    /**
+     * adds task to queue
+     * @param task Task object to be added to queue
+     * @throws DuplicateElementException If task already in queue
+     */
     public void add(Task task) throws DuplicateElementException {
         if (isFull()){
             throw new IllegalStateException("Queue is full!");
@@ -65,7 +94,12 @@ public class BoundedPriorityQueueSet {
         queue.add(calcPosition(task), task);
     }
 
-
+    /**
+     * also an add but returns true or false
+     * @param task Task object to be added
+     * @return True if added or False if not added
+     * @throws DuplicateElementException Thrown if task already in queue
+     */
     public boolean offer(Task task) throws DuplicateElementException {
         if (isFull() || isDuplicate(task)){
             return false;
@@ -74,7 +108,10 @@ public class BoundedPriorityQueueSet {
         return true;
     }
 
-
+    /**
+     * gets copy of first element in queue
+     * @return Task which was copied or a NoSuchElementException if queue is empty
+     */
     public Task element(){
         if (isEmpty()){
             throw new NoSuchElementException("Queue Empty!");
@@ -83,7 +120,10 @@ public class BoundedPriorityQueueSet {
         return task2;
     }
 
-
+    /**
+     * gets copy of first element in queue
+     * @return the copied task or null if queue is empty
+     */
     public Task peek(){
         if (isEmpty()){
             return null;
@@ -93,6 +133,10 @@ public class BoundedPriorityQueueSet {
     }
 
 
+    /**
+     * removes first task in queue
+     * @return Task which was removed or NoSuchElementException exception if queue is empty
+     */
     public Task remove(){
         if (isEmpty()){
             throw new NoSuchElementException("Queue Empty!");
@@ -102,7 +146,10 @@ public class BoundedPriorityQueueSet {
         return task2;
     }
 
-
+    /**
+     * Removes first task in queue
+     * @return Task which was removed or null if queue is empty
+     */
     public Task poll(){
         if (isEmpty()){
             return null;

691fa9781dee93bbfd2f7076187d59c06747a37a
smath <smathewslynch@gmail.com>
2024-04-17 18:52:36 +0100

    Added javaDocs files

---
+++ JavaDocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"<Unnamed>","l":"BoundedPriorityQueueSet"},{"p":"<Unnamed>","l":"DuplicateElementException"},{"p":"<Unnamed>","l":"Menu"},{"p":"<Unnamed>","l":"Task"}];updateSearchResults();
